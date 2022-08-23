baa5af3dada6efb4f0e4f043246b00fc34e3bc1d
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-23 14:01:34 -0400

    Added messenger server to project

---
+++ messengerserver/src/main/java/dev/haskin/messengerserver/MessengerserverApplication.java
@@ -0,0 +1,13 @@
+package dev.haskin.messengerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerserverApplication.class, args);
+	}
+
+}
---
+++ messengerserver/src/test/java/dev/haskin/messengerserver/MessengerserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.haskin.messengerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
