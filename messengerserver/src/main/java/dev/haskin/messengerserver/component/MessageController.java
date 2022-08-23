315feadc20dcb6e3c9c61a8a7d34b0eebe005573
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-23 15:57:16 -0400

    Added Cross Origin to server

--- messengerserver/src/main/java/dev/haskin/messengerserver/component/MessageController.java
+++ messengerserver/src/main/java/dev/haskin/messengerserver/component/MessageController.java
@@ -3,6 +3,7 @@ package dev.haskin.messengerserver.component;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ public class MessageController {
     @Autowired
     private MessageService messageService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/get-user-message")
     List<Message> getUserMessage() {
         return messageService.getUserMessages();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/get-sender-message")
     List<Message> getSenderMessage() {
         return messageService.getSenderMessages();
