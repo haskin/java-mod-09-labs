2b5a86f4ae5537923f405040ba4cb15a58b0fad3
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-23 13:18:42 -0400

    added messaging service logic

--- messenger-app/src/app/service/messaging-data.service.ts
+++ messenger-app/src/app/service/messaging-data.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import Message from '../model/Message';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class MessagingDataService {
+  private senderMessages: Message[] = [
+    {
+      sender: { firstName: 'Ludovic', isOnline: true },
+      text: 'Message from Ludovic',
+      conversationId: 1,
+      sequenceNumber: 0,
+    },
+    {
+      sender: { firstName: 'Jessica' },
+      text: 'Message from Jessica',
+      conversationId: 1,
+      sequenceNumber: 1,
+    },
+  ];
 
-  constructor() { }
+  private userMessages: Message[] = [
+    {
+      sender: { firstName: 'Aurelie' },
+      text: 'Message from Aurelie',
+      conversationId: 1,
+      sequenceNumber: 2,
+    },
+  ];
+
+  userMessagesChanged = new EventEmitter<Message[]>();
+
+  getSenderMessages() {
+    return this.senderMessages.slice();
+  }
+
+  getUserMessages() {
+    return this.userMessages.slice();
+  }
+
+  addUserMessage(newMessage: Message) {
+    this.userMessages.push(newMessage);
+    this.userMessagesChanged.emit(this.userMessages.slice());
+  }
+
+  constructor() {}
 }
