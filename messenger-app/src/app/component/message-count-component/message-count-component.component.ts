e8d46f528c63746ec4b905b5679da8df65b55b23
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-23 13:40:29 -0400

    Added MessagingDataService logic to message count component

--- messenger-app/src/app/component/message-count-component/message-count-component.component.ts
+++ messenger-app/src/app/component/message-count-component/message-count-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MessagingDataService } from 'src/app/service/messaging-data.service';
+import Message from 'src/app/model/Message';
 
 @Component({
   selector: 'app-message-count-component',
@@ -6,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-count-component.component.css'],
 })
 export class MessageCountComponentComponent implements OnInit {
-  sentMessageCount?: number;
+  sentMessageCount: number = 0;
+  userMessageCount: number = 0;
+  senderMessageCount: number = 0;
 
-  constructor() {}
+  constructor(private messageService: MessagingDataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userMessageCount = this.messageService.getUserMessages().length;
+    this.senderMessageCount = this.messageService.getSenderMessages().length;
+    this.sentMessageCount = this.userMessageCount + this.senderMessageCount;
+
+    this.messageService.userMessagesChanged.subscribe((messages) => {
+      this.sentMessageCount -= this.userMessageCount;
+      this.sentMessageCount += messages.length;
+    });
+
+    this.messageService.senderMessagesChanged.subscribe((messages) => {
+      this.sentMessageCount -= this.senderMessageCount;
+      this.sentMessageCount += messages.length;
+    });
+  }
 }
