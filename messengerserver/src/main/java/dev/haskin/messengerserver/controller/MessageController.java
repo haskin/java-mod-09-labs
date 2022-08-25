b7fd76afd07ac904dc36ac6e423f497b38f55c4c
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-25 11:53:37 -0400

    created preliminary delete mapping in controller

--- messengerserver/src/main/java/dev/haskin/messengerserver/controller/MessageController.java
+++ messengerserver/src/main/java/dev/haskin/messengerserver/controller/MessageController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +20,19 @@ public class MessageController {
 
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/get-user-messages")
-    List<Message> getUserMessage() {
+    public List<Message> getUserMessage() {
         return messageService.getUserMessages();
     }
 
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/get-sender-messages")
-    List<Message> getSenderMessage() {
+    public List<Message> getSenderMessage() {
         return messageService.getSenderMessages();
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @DeleteMapping(value = "/delete-message")
+    public void deleteMessage(Message message) {
+        return;
     }
 }
